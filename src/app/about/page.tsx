a4689ac3f18d4848b18bb4c876f275b248f9cf99
Evan Lin <62809897+eevaain@users.noreply.github.com>
2024-04-10 23:56:09 -0400

    updated aboutme

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,32 +9,47 @@ export default function About() {
       <h1 className="text-4xl font-bold mb-2">evanlin.ca</h1>
       <Navigation />
       <div className="flex flex-col p-10 gap-4 w-full">
-      <h1 className="text-xl md:text-2xl underline">About Me</h1>
+        <h1 className="text-xl md:text-2xl underline">About Me</h1>
 
         <p>Hi! I'm Evan.</p>
         <p>
-          I{"'"}m fascinated by the fundamental building blocks of our world and how
-          they make up the black boxes in our everyday lives.
+          I{"'"}m fascinated by the fundamental building blocks of our world and
+          how they make up the black boxes in our everyday lives.
         </p>
         <p>
           Even more so, I feel inclined to <span className="italic">open</span>{" "}
           them.
         </p>
         <p>
-          I also love telling stories. Especially making people excited about things
-          they didn{"'"}t know they could feel excited about.
+          I also love telling stories. Especially making people excited about
+          things they didn{"'"}t know they could feel excited about.
         </p>
+        <p>Youtube used to be this medium for me.</p>
         <p>
-          Youtube used to be this medium for me. 
+          Nowadays I try to share the things I do through{" "}
+          <a
+            href="https://twitter.com/evannlinn"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" text-blue-500"
+          >
+            Twitter,
+          </a> or more recently my{" "}
+          <a
+            href="https://evanlin.ca/writing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" text-blue-500"
+          >
+            writing
+          </a>
+          .
         </p>
+
+        <p>I think I'll return to making videos someday. Maybe soon.</p>
         <p>
-          But nowaways I try to share the things I do through Twitter {"(X)"} or my writing.  
-        </p>
-        <p>
-          I think I'll return to this someday. Maybe soon.   
-        </p>
-        <p>
-          After all, shouldn't we all be working on things which we enjoy and come natural to us? 
+          After all, shouldn't we all be working on things which we enjoy and
+          come natural to us?
         </p>
       </div>
     </div>
